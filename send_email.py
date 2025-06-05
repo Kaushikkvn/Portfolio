67b46464823855561ae3946f6c8f04744375d180
Kaushik <kaushik.kommineni@gmail.com>
2025-06-05 18:47:20 -0500

    edits

--- send_email.py
+++ send_email.py
@@ -3,16 +3,18 @@ import smtplib
 import ssl
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
 load_dotenv()
 
 def send_email(message):
-    host="smtp.gmail.com"
-    port=465
-    username="venkatakommineni30@gmail.com"
-    password=os.getenv("PASSWORD")
-    receiver="venkatakommineni30@gmail.com"
-    context=ssl.create_default_context()
-    with smtplib.SMTP_SSL(host,port,context=context) as server:
-        server.login(username,password)
-        server.sendmail(username,receiver,message)
+    host = "smtp-relay.brevo.com"
+    port = 587
+    username = os.getenv("BREVO_USERNAME")
+    password = os.getenv("BREVO_PASSWORD")
+    receiver = "venkatakommineni30@gmail.com"
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP(host, port) as server:
+        server.starttls(context=context)
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+        print("✅ Email sent!")
