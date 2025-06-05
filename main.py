49e445e1cc3f02c88bf2ba5a59e6cb9aeadbc86d
Kaushik <kaushik.kommineni@gmail.com>
2025-06-05 00:47:59 -0500

    added titles to the web page

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import streamlit as st
-from PIL.ImageFont import Layout
+import pandas
 
 st.set_page_config(layout="wide")
 col1,col2=st.columns(2)
 
 with col1:
     st.image("images/image.jpeg",width=350)
-with col2:
+with (col2):
     st.title("Kaushik Kommineni")
     content="""
     Data-driven Computer Science graduate with strong analytical, 
@@ -16,3 +16,17 @@ with col2:
     Seeking a Data Analyst role to leverage my expertise in statistical analysis, data visualization, and machine learning.
     """
     st.info(content)
+content2="""
+Below you can find some of the apps I have built in python. Feel free to contact me! 
+"""
+st.write(content2)
+col3,col4=st.columns(2)
+
+df=pandas.read_csv("data.csv",sep=";")
+with col3:
+    for index,row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index,row in df[10:].iterrows():
+        st.header(row["title"])
