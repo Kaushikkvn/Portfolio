0f7c7e97f680bd61e89f844efc281eb520cd642c
Kaushik <kaushik.kommineni@gmail.com>
2025-06-05 01:56:15 -0500

    sending emails

---
+++ send_email.py
@@ -0,0 +1,15 @@
+import smtplib
+import ssl
+
+
+
+def send_email(message):
+    host="smtp.gmail.com"
+    port=465
+    username="venkatakommineni30@gmail.com"
+    password="udqo zsdl hkrf nccz"
+    receiver="venkatakommineni30@gmail.com"
+    context=ssl.create_default_context()
+    with smtplib.SMTP_SSL(host,port,context=context) as server:
+        server.login(username,password)
+        server.sendmail(username,receiver,message)
