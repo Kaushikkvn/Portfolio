02225b184ccb6876d6bf8f2fe97d1c790ae586a2
Kaushik <kaushik.kommineni@gmail.com>
2025-06-05 19:33:37 -0500

    changes i am have made5

--- pages/Contact_me.py
+++ pages/Contact_me.py
@@ -1,12 +1,12 @@
 import streamlit as st
 from send_email import send_email
 
+st.set_page_config(page_title="Contact Me", page_icon="📬")
 st.header("📬 Contact Me")
 
 with st.form(key="email_form"):
-    user_email = st.text_input("Your email address")
-    raw_message = st.text_area("Your message")
-
+    user_email = st.text_input("Your email address", placeholder="you@example.com")
+    raw_message = st.text_area("Your message", height=150, placeholder="What would you like to say?")
     submit_button = st.form_submit_button("Send")
 
     if submit_button:
