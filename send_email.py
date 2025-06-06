a8698fe5be8726dde21b22f440238b99a7787a78
Kaushik <kaushik.kommineni@gmail.com>
2025-06-05 19:33:37 -0500

    changes i am have made5

--- send_email.py
+++ send_email.py
@@ -1,18 +1,20 @@
 import requests
 
 def send_email(message, user_email):
-    form_url = "https://formsubmit.co/el/juhuge"
+    form_url = "https://formsubmit.co/el/juhuge"  # your verified endpoint
 
     headers = {
         "Content-Type": "application/x-www-form-urlencoded"
     }
 
     data = {
-        "name": "Streamlit User",
+        "name": "Streamlit Visitor",
         "email": user_email,
         "message": message,
+        "_autoresponse": "Thanks for contacting me! I'll get back to you soon.",
         "_captcha": "false",
-        "_template": "table"
+        "_template": "table",
+        "_next": "https://portfolio-kaushikkommineni.streamlit.app/Contact_me"
     }
 
     response = requests.post(form_url, data=data, headers=headers)
