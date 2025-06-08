59acc2b3324a6d5e3782d73e0337e431cda731c6
Kaushik <kaushik.kommineni@gmail.com>
2025-06-07 21:37:09 -0500

    final commit is done

--- Home.py
+++ Home.py
@@ -90,14 +90,14 @@ elif selected == "Projects":
     col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
     with col3:
-        for index, row in df[:10].iterrows():
+        for index, row in df[:6].iterrows():
             st.header(row["title"])
             st.write(row["description"])
             st.image("images/" + row["image"])
             st.write(f"[Source Code]({row['url']})")
 
     with col4:
-        for index, row in df[10:].iterrows():
+        for index, row in df[6:].iterrows():
             st.header(row["title"])
             st.write(row["description"])
             st.image("images/" + row["image"])
